bb6c4e403acbe5948b3817012f133222100ca06d
Mathias Rieder <mathias.rieder@gmail.com>
2026-04-23 07:00:08 +0000

    added todo

--- argv.c
+++ argv.c
@@ -2,6 +2,8 @@
 
 int main(int argc, char *argv[]) {
 
+  // TODO print the number of arguments and all the arguments themselves
+
   printf("What the heck are these argc and argv?\n");
   printf("Why is one an int and one an array?\n");
   
