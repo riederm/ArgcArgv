9f95b64161277bee318ba47f268dc263e5eb8d06
Mathias Rieder <mathias.rieder@gmail.com>
2026-04-23 06:54:21 +0000

    print the number of arguments

--- argv.c
+++ argv.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 int main(int argc, char *argv[]) {
-  
+
+  printf("Number of command-line arguments: %d\n", argc);
   printf("Command-line arguments:\n");
   for (int i = 0; i < argc; i++) {
     printf("%d. %s\n", i, argv[i]);
